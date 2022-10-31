90fc7eca8b4fee62668e763a3244f647733ee79e
Yari-Carelli <yaricarelli@hotmail.it>
2022-10-31 15:03:50 +0000

    correct axiosDefault.js

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+axios.defaults.baseURL = "https://drf-api-yc.herokuapp.com/";
+axios.defaults.baseURL = "https://8000-yaricarelli-drfapi-rltrsnyberh.ws-eu73.gitpod.io/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
